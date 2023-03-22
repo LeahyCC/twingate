73ca02a56171ad6894af9cdc956905a51b13d5df
Colin Leahy <ccleahy2020@gmail.com>
2023-03-21 18:57:59 -0700

    add missing ac

--- src/App.tsx
+++ src/App.tsx
@@ -6,39 +6,39 @@ import Generated from './components/Generated/Generated'
 import './reset.css'
 import * as styles from './App.styles'
 
-// const DUMMY_JSON = `
-// {
-//   "type": "hero",
-//   "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc"
-// },
-// {
-//   "type": "image-text",
-//   "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
-//   "text": "Sunset from the sky......",
-//   "title": "Airplane",
-//   "leftToRight": false
-// },
-// {
-//   "type": "image-text",
-//   "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
-//   "text": "Sunset from the sky......",
-//   "title": "Airplane",
-//   "leftToRight": true
-// },
-// {
-//   "type": "image-text",
-//   "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
-//   "text": "Sunset from the sky......",
-//   "title": "Airplane"
-// },
-// {
-//   "type": "data",
-//   "url": "https://api.publicapis.org/entries"
-// }
-// `
+const DUMMY_JSON = `
+{
+  "type": "hero",
+  "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc"
+},
+{
+  "type": "image-text",
+  "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
+  "text": "Sunset from the sky......",
+  "title": "Airplane",
+  "leftToRight": false
+},
+{
+  "type": "image-text",
+  "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
+  "text": "Sunset from the sky......",
+  "title": "Airplane",
+  "leftToRight": true
+},
+{
+  "type": "image-text",
+  "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
+  "text": "Sunset from the sky......",
+  "title": "Airplane"
+},
+{
+  "type": "data",
+  "url": "https://api.publicapis.org/entries"
+}
+`
 
-// const INITIAL_JSON = `[${DUMMY_JSON}]`
-const INITIAL_JSON = `[]`
+const INITIAL_JSON = `[${DUMMY_JSON}]`
+// const INITIAL_JSON = `[]`
 
 function App() {
   const [jsonInput, setJsonInput] = useState(INITIAL_JSON)
