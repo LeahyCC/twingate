470ab610cf68984aef938aadcfc4ccff149e7c37
Colin Leahy <ccleahy2020@gmail.com>
2023-03-27 14:00:02 -0700

    refactor a bit

--- src/App.tsx
+++ src/App.tsx
@@ -6,56 +6,60 @@ import Generated from './components/Generated/Generated'
 import './reset.css'
 import * as styles from './App.styles'
 
-// const DUMMY_JSON = `
-// {
-//   "type": "hero",
-//   "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc"
-// },
-// {
-//   "type": "image-text",
-//   "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
-//   "text": "Sunset from the sky......",
-//   "title": "Airplane",
-//   "leftToRight": false
-// },
-// {
-//   "type": "image-text",
-//   "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
-//   "text": "Sunset from the sky......",
-//   "title": "Airplane",
-//   "leftToRight": true
-// },
-// {
-//   "type": "image-text",
-//   "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
-//   "text": "Sunset from the sky......",
-//   "title": "Airplane"
-// },
-// {
-//   "type": "data",
-//   "url": "https://api.publicapis.org/entries"
-// }
-// `
+const DUMMY_JSON = `
+{
+  "type": "hero",
+  "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc"
+},
+{
+  "type": "image-text",
+  "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
+  "text": "Sunset from the sky......",
+  "title": "Airplane",
+  "leftToRight": false
+},
+{
+  "type": "image-text",
+  "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
+  "text": "Sunset from the sky......",
+  "title": "Airplane",
+  "leftToRight": true
+},
+{
+  "type": "image-text",
+  "imageURI": "https://images.unsplash.com/photo-1579963333765-b4129b3250fc",
+  "text": "Sunset from the sky......",
+  "title": "Airplane"
+},
+{
+  "type": "data",
+  "url": "https://api.publicapis.org/entries"
+}
+`
 
-// const INITIAL_JSON = `[${DUMMY_JSON}]`
-const INITIAL_JSON = `[]`
+const INITIAL_JSON = `[${DUMMY_JSON}]`
+// const INITIAL_JSON = `[]`
+
+export type JsonState = {
+  isValid: boolean
+  isArray: boolean
+  json: string
+}
 
 function App() {
-  const [jsonInput, setJsonInput] = useState(INITIAL_JSON)
-  const [isValidJson, setIsValidJson] = useState(true)
+  const [jsonState, setJsonState] = useState({
+    isValid: true,
+    isArray: true,
+    json: INITIAL_JSON,
+  })
 
   return (
     <div css={styles.App}>
       <div css={styles.TextEditorContainer}>
-        <TextEditor
-          jsonInput={jsonInput}
-          setJsonInput={setJsonInput}
-          setIsValidJson={setIsValidJson}
-        />
+        <TextEditor jsonState={jsonState} setJsonState={setJsonState} />
       </div>
       <div css={styles.GeneratedContainer}>
-        {isValidJson && <Generated jsonInput={jsonInput} />}
-        {!isValidJson && <div>Invalid JSON or empty input field</div>}
+        <Generated jsonState={jsonState} />
       </div>
     </div>
   )
