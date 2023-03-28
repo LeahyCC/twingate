f72bee62de43d5944aa4018d70dc48a54a999592
Colin Leahy <ccleahy2020@gmail.com>
2023-03-27 17:33:01 -0700

    refactor 2

--- src/components/Generated/ElementGenerator.tsx
+++ src/components/Generated/ElementGenerator.tsx
@@ -1,66 +1,73 @@
-import { useEffect } from 'react'
-import { ElementTypes, heroElement, dataElement, imageTextElement } from './generated-config'
+import { useEffect, useCallback, memo, useState } from 'react'
+import {
+  ElementTypes,
+  ElementsProps,
+  ElementGeneratorsProps,
+  HeroElementProps,
+  DataElementProps,
+  ImageTextElementProps,
+} from './generated-config'
 
 // note: ideally separate styles into their own file
 import * as styles from './Generated.styes'
 
-type ElementProps = heroElement | imageTextElement | dataElement
+const Hero = memo(function Hero({ imageURI }: HeroElementProps) {
+  return (
+    <div css={styles.HeroImage}>
+      <img src={imageURI} alt="Hero Image" />
+    </div>
+  )
+})
 
-type ElementGeneratorProps = (heroElement | imageTextElement | dataElement) & {
-  jsonData: string
-  setJsonData: (data: string) => void
+const ImageText = memo(function ImageText({
+  leftToRight = true,
+  imageURI,
+  title,
+  text,
+}: ImageTextElementProps) {
+  const flexDirection = leftToRight ? 'row' : 'row-reverse'
+  return (
+    <div css={[styles.ImageText, { flexDirection }]}>
+      {imageURI && <img src={imageURI} alt="Image" />}
+      <div>
+        {title && <h3>{title}</h3>}
+        <p>{text}</p>
+      </div>
+    </div>
+  )
+})
+
+const DataElement = memo(function DataElement({ url }: DataElementProps & ElementsProps) {
+  const [jsonData, setJsonData] = useState('')
+
+  useEffect(() => {
+    refreshData()
+  }, [])
+
+  const refreshData = useCallback(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setJsonData(JSON.stringify(data).slice(0, 100) + '...'))
+  }, [url])
+
+  return (
+    <div>
+      <pre css={styles.pre}>{jsonData}</pre>
+      <br />
+      <button onClick={refreshData}>refresh</button>
+    </div>
+  )
+})
+
+const elementGenerators: Record<ElementTypes, ElementGeneratorsProps> = {
+  hero: (elementProps: ElementsProps) => <Hero {...(elementProps as HeroElementProps)} />,
+  'image-text': (elementProps: ElementsProps) => (
+    <ImageText {...(elementProps as ImageTextElementProps)} />
+  ),
+  data: (elementProps: ElementsProps) => <DataElement {...(elementProps as DataElementProps)} />,
 }
 
-const ElementGenerator = (props: ElementGeneratorProps) => {
-  const Hero = ({ imageURI }: heroElement) => {
-    return (
-      <div css={styles.HeroImage}>
-        <img src={imageURI} alt="Hero Image" />
-      </div>
-    )
-  }
-
-  const ImageText = ({ leftToRight = true, imageURI, title, text }: imageTextElement) => {
-    const flexDirection = leftToRight ? 'row' : 'row-reverse'
-    return (
-      <div css={[styles.ImageText, { flexDirection }]}>
-        {imageURI && <img src={imageURI} alt="Image" />}
-        <div>
-          {title && <h3>{title}</h3>}
-          <p>{text}</p>
-        </div>
-      </div>
-    )
-  }
-
-  const DataElement = ({ url }: dataElement) => {
-    useEffect(() => {
-      refreshData()
-    }, [url])
-
-    const refreshData = () => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => props.setJsonData(JSON.stringify(data).slice(0, 100) + '...'))
-    }
-
-    return (
-      <div>
-        <pre css={styles.pre}>{props.jsonData}</pre>
-        <br />
-        <button onClick={refreshData}>refresh</button>
-      </div>
-    )
-  }
-
-  const elementGenerators: Record<ElementTypes, (elementProps: ElementProps) => JSX.Element> = {
-    hero: (elementProps: ElementProps) => <Hero {...(elementProps as heroElement)} />,
-    'image-text': (elementProps: ElementProps) => (
-      <ImageText {...(elementProps as imageTextElement)} />
-    ),
-    data: (elementProps: ElementProps) => <DataElement {...(elementProps as dataElement)} />,
-  }
-
+const ElementGenerator = (props: ElementsProps) => {
   const ElementComponent = elementGenerators[props.type]
 
   if (!ElementComponent) {
@@ -70,4 +77,4 @@ const ElementGenerator = (props: ElementGeneratorProps) => {
   return <ElementComponent {...props} />
 }
 
-export default ElementGenerator
+export default memo(ElementGenerator)
--- src/components/Generated/generated-config.ts
+++ src/components/Generated/generated-config.ts
@@ -1,18 +1,25 @@
-export type heroElement = {
+export type HeroElementProps = {
   type: 'hero'
   imageURI: string
 }
-export type imageTextElement = {
+
+export type ImageTextElementProps = {
   type: 'image-text'
   imageURI: string
   text: string
   title?: string
   leftToRight?: boolean
 }
-export type dataElement = {
+
+export type DataElementProps = {
   type: 'data'
   url: string
+  setJsonData: (data: string) => void
+  jsonData: string
 }
 
-export type Element = heroElement | imageTextElement | dataElement
 export type ElementTypes = 'hero' | 'image-text' | 'data'
+
+export type ElementsProps = HeroElementProps | ImageTextElementProps | DataElementProps
+
+export type ElementGeneratorsProps = (elementProps: ElementsProps) => JSX.Element
